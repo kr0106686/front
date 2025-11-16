dcdd4478240b268d5566e3d9f4f18f8c4252c728
jackie <kr0106686@gmail.com>
2025-11-17 06:56:24 +0900

    front 20251117065623

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/kr0106686/front
 
 go 1.25.3
 
-require github.com/stretchr/testify v1.11.1
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/joho/godotenv v1.5.1
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require github.com/kr0106686/public v0.0.0-20251116203353-ae417d1eaccc
--- main.go
+++ main.go
@@ -1,53 +1,40 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
-	"path"
-	"text/template"
+	"os"
+
+	"github.com/kr0106686/public/filer"
 )
 
-var staticDir = flag.String("static_dir", "static", "스태틱 폴더 경로")
-var socketURL = flag.String("socket_url", "ws://localhost:5555/ws", "소켓 경로")
-var port = flag.String("port", "5555", "포트")
-
-func renderHTML(layoutTemplatePath string) *template.Template {
-	return template.Must(template.ParseGlob(layoutTemplatePath))
-}
+var socketURL = os.Getenv("SOCKET_URL")
+var port = os.Getenv("FRONT_PORT")
+var staticDir = "/static"
 
 func main() {
-	flag.Parse()
+	f := filer.New()
 
-	layoutTemplatePath := path.Join(*staticDir, "templates", "layout", "*.html")
-	pageTemplateDir := path.Join(*staticDir, "templates", "page")
-
-	layoutTmpl := renderHTML(layoutTemplatePath)
-
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(*staticDir))))
-
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		layoutTmpl.ParseFiles(path.Join(pageTemplateDir, "home.html"))
-		layoutTmpl.ExecuteTemplate(w, "layout", map[string]interface{}{
+		f.ReadHtmlTmpl(w, "home", map[string]interface{}{
 			"title": "home",
 		})
 	})
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		layoutTmpl.ParseFiles(path.Join(pageTemplateDir, "login.html"))
-		layoutTmpl.ExecuteTemplate(w, "layout", map[string]interface{}{
+		f.ReadHtmlTmpl(w, "login", map[string]interface{}{
 			"title": "login",
 		})
 	})
 
 	http.HandleFunc("/omok", func(w http.ResponseWriter, r *http.Request) {
-		layoutTmpl.ParseFiles(path.Join(pageTemplateDir, "omok.html"))
-		layoutTmpl.ExecuteTemplate(w, "layout", map[string]interface{}{
+		f.ReadHtmlTmpl(w, "login", map[string]interface{}{
 			"title":     "omok",
-			"socketURL": *socketURL,
+			"socketURL": socketURL,
 		})
 	})
 
-	log.Printf("http://localhost:%s", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	log.Printf("http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
